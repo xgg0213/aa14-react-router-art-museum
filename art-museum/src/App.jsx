9e895bec432a302aa42bc5c78b966f42eed521c1
xgg0213 <m.eringan@gmail.com>
2024-10-03 21:56:36 -0700

    phase 1 completed

--- art-museum/src/App.jsx
+++ art-museum/src/App.jsx
@@ -5,6 +5,14 @@ import { harvardArt } from './data/harvardArt';
 
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: 
+    <>
+      <h2>Harvard Art Museum</h2>
+      <p>Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.</p>
+    </>
+  },
   { 
     path: "*",
     element: <h2>Page Not Found</h2>
